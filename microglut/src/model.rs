5a7ff56e4677f0e7c3c6c48399be1b09e9dfc327
KingTheFifth <johannes.kung@live.com>
2025-04-02 18:58:38 +0200

    Change Material in microglut to use the standard MTL attributes

--- microglut/src/model.rs
+++ microglut/src/model.rs
@@ -4,7 +4,7 @@ use std::{collections::HashMap, io::BufReader, path::Path};
 use glam::{Vec2, Vec3};
 use glow::{
     Buffer, Context, HasContext as _, NativeProgram, VertexArray, ARRAY_BUFFER,
-    ELEMENT_ARRAY_BUFFER, FLOAT, STATIC_DRAW, TEXTURE_2D, TRIANGLES, UNSIGNED_INT,
+    ELEMENT_ARRAY_BUFFER, FLOAT, STATIC_DRAW, TEXTURE0, TEXTURE_2D, TRIANGLES, UNSIGNED_INT,
 };
 
 use crate::Texture;
@@ -187,36 +187,269 @@ impl Mesh {
 
 #[derive(Debug, Clone, Copy)]
 struct Material {
-    texture: Option<Texture>,
-    k_d: Option<Vec3>,
+    ambient: Option<Vec3>,
+    diffuse: Option<Vec3>,
+    specular: Option<Vec3>,
+    shininess: Option<f32>,
+    dissolve: Option<f32>,
+    optical_density: Option<f32>,
+    ambient_texture: Option<Texture>,
+    diffuse_texture: Option<Texture>,
+    specular_texture: Option<Texture>,
+    normal_texture: Option<Texture>,
+    shininess_texture: Option<Texture>,
+    dissolve_texture: Option<Texture>,
+    illumination_model: Option<u8>,
+}
+
+pub struct MaterialBindings {
+    pub ambient: Option<String>,
+    pub diffuse: Option<String>,
+    pub specular: Option<String>,
+    pub shininess: Option<String>,
+    pub dissolve: Option<String>,
+    pub optical_density: Option<String>,
+    pub ambient_texture: Option<(String, u32)>,
+    pub diffuse_texture: Option<(String, u32)>,
+    pub specular_texture: Option<(String, u32)>,
+    pub normal_texture: Option<(String, u32)>,
+    pub shininess_texture: Option<(String, u32)>,
+    pub dissolve_texture: Option<(String, u32)>,
+    pub illumination_model: Option<String>,
 }
 
 impl Material {
     fn new(gl: &Context, material: tobj::Material, texture_loader: Option<&TextureLoader>) -> Self {
         // TODO: is the "main" texture always the name?
-        let texture = texture_loader
-            .map(|f| f(&material.name))
-            .map(|data| Texture::load(gl, &data, true));
-        Material {
-            texture,
-            k_d: material.diffuse.map(Vec3::from_array),
+        if let Some(tex_loader) = texture_loader {
+            let ambient_texture = material
+                .ambient_texture
+                .map(|texture_name| tex_loader(&texture_name))
+                .map(|data| Texture::load(gl, &data, true));
+            let diffuse_texture = material
+                .diffuse_texture
+                .map(|texture_name| tex_loader(&texture_name))
+                .map(|data| Texture::load(gl, &data, true));
+            let specular_texture = material
+                .specular_texture
+                .map(|texture_name| tex_loader(&texture_name))
+                .map(|data| Texture::load(gl, &data, true));
+            let normal_texture = material
+                .normal_texture
+                .map(|texture_name| tex_loader(&texture_name))
+                .map(|data| Texture::load(gl, &data, true));
+            let shininess_texture = material
+                .shininess_texture
+                .map(|texture_name| tex_loader(&texture_name))
+                .map(|data| Texture::load(gl, &data, true));
+            let dissolve_texture = material
+                .dissolve_texture
+                .map(|texture_name| tex_loader(&texture_name))
+                .map(|data| Texture::load(gl, &data, true));
+            Material {
+                ambient: material.ambient.map(Vec3::from_array),
+                diffuse: material.diffuse.map(Vec3::from_array),
+                specular: material.specular.map(Vec3::from_array),
+                shininess: material.shininess,
+                dissolve: material.dissolve,
+                optical_density: material.optical_density,
+                ambient_texture,
+                diffuse_texture,
+                specular_texture,
+                normal_texture,
+                shininess_texture,
+                dissolve_texture,
+                illumination_model: material.illumination_model,
+            }
+        } else {
+            Material {
+                ambient: material.ambient.map(Vec3::from_array),
+                diffuse: material.diffuse.map(Vec3::from_array),
+                specular: material.specular.map(Vec3::from_array),
+                shininess: material.shininess,
+                dissolve: material.dissolve,
+                optical_density: material.optical_density,
+                ambient_texture: None,
+                diffuse_texture: None,
+                specular_texture: None,
+                normal_texture: None,
+                shininess_texture: None,
+                dissolve_texture: None,
+                illumination_model: material.illumination_model,
+            }
         }
     }
 
-    fn bind(&self, gl: &Context, program: NativeProgram) {
+    fn bind(&self, gl: &Context, program: NativeProgram, bindings: &MaterialBindings) {
         unsafe {
-            // TODO: configure name
-            if let Some(k_d) = &self.k_d {
-                gl.uniform_3_f32(
-                    gl.get_uniform_location(program, "k_d").as_ref(),
-                    k_d.x,
-                    k_d.y,
-                    k_d.z,
+            if let Some(ambient_binding) = &bindings.ambient {
+                gl.uniform_3_f32_slice(
+                    gl.get_uniform_location(program, &ambient_binding).as_ref(),
+                    self.ambient.unwrap_or_default().as_ref(),
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", ambient_binding))
+                        .as_ref(),
+                    self.ambient.is_some() as i32,
                 );
             }
-            if let Some(tex) = &self.texture {
-                // TODO: specify texture unit
-                gl.bind_texture(TEXTURE_2D, Some(tex.id()));
+            if let Some(diffuse_binding) = &bindings.diffuse {
+                gl.uniform_3_f32_slice(
+                    gl.get_uniform_location(program, &diffuse_binding).as_ref(),
+                    self.diffuse.unwrap_or_default().as_ref(),
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", diffuse_binding))
+                        .as_ref(),
+                    self.diffuse.is_some() as i32,
+                );
+            }
+            if let Some(specular_binding) = &bindings.specular {
+                gl.uniform_3_f32_slice(
+                    gl.get_uniform_location(program, &specular_binding).as_ref(),
+                    self.specular.unwrap_or_default().as_ref(),
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", specular_binding))
+                        .as_ref(),
+                    self.specular.is_some() as i32,
+                );
+            }
+            if let Some(shininess_binding) = &bindings.shininess {
+                gl.uniform_1_f32(
+                    gl.get_uniform_location(program, &shininess_binding)
+                        .as_ref(),
+                    self.shininess.unwrap_or_default(),
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", shininess_binding))
+                        .as_ref(),
+                    self.shininess.is_some() as i32,
+                );
+            }
+            if let Some(dissolve_binding) = &bindings.dissolve {
+                gl.uniform_1_f32(
+                    gl.get_uniform_location(program, &dissolve_binding).as_ref(),
+                    self.dissolve.unwrap_or_default(),
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", dissolve_binding))
+                        .as_ref(),
+                    self.dissolve.is_some() as i32,
+                );
+            }
+            if let Some(optical_density_binding) = &bindings.optical_density {
+                gl.uniform_1_f32(
+                    gl.get_uniform_location(program, &optical_density_binding)
+                        .as_ref(),
+                    self.optical_density.unwrap_or_default(),
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", optical_density_binding))
+                        .as_ref(),
+                    self.optical_density.is_some() as i32,
+                );
+            }
+
+            if let Some((ambient_texture_binding, texture_unit)) = &bindings.ambient_texture {
+                gl.active_texture(TEXTURE0 + texture_unit);
+                gl.bind_texture(TEXTURE_2D, self.ambient_texture.map(|t| t.id()));
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &ambient_texture_binding)
+                        .as_ref(),
+                    *texture_unit as i32,
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", ambient_texture_binding))
+                        .as_ref(),
+                    self.ambient_texture.is_some() as i32,
+                );
+            }
+            if let Some((diffuse_texture_binding, texture_unit)) = &bindings.diffuse_texture {
+                gl.active_texture(TEXTURE0 + texture_unit);
+                gl.bind_texture(TEXTURE_2D, self.diffuse_texture.map(|t| t.id()));
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &diffuse_texture_binding)
+                        .as_ref(),
+                    *texture_unit as i32,
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", diffuse_texture_binding))
+                        .as_ref(),
+                    self.diffuse_texture.is_some() as i32,
+                );
+            }
+            if let Some((specular_texture_binding, texture_unit)) = &bindings.specular_texture {
+                gl.active_texture(TEXTURE0 + texture_unit);
+                gl.bind_texture(TEXTURE_2D, self.specular_texture.map(|t| t.id()));
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &specular_texture_binding)
+                        .as_ref(),
+                    *texture_unit as i32,
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", specular_texture_binding))
+                        .as_ref(),
+                    self.specular_texture.is_some() as i32,
+                );
+            }
+            if let Some((normal_texture_binding, texture_unit)) = &bindings.normal_texture {
+                gl.active_texture(TEXTURE0 + texture_unit);
+                gl.bind_texture(TEXTURE_2D, self.normal_texture.map(|t| t.id()));
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &normal_texture_binding)
+                        .as_ref(),
+                    *texture_unit as i32,
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", normal_texture_binding))
+                        .as_ref(),
+                    self.normal_texture.is_some() as i32,
+                );
+            }
+            if let Some((shininess_texture_binding, texture_unit)) = &bindings.shininess_texture {
+                gl.active_texture(TEXTURE0 + texture_unit);
+                gl.bind_texture(TEXTURE_2D, self.shininess_texture.map(|t| t.id()));
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &shininess_texture_binding)
+                        .as_ref(),
+                    *texture_unit as i32,
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", shininess_texture_binding))
+                        .as_ref(),
+                    self.shininess_texture.is_some() as i32,
+                );
+            }
+            if let Some((dissolve_texture_binding, texture_unit)) = &bindings.dissolve_texture {
+                gl.active_texture(TEXTURE0 + texture_unit);
+                gl.bind_texture(TEXTURE_2D, self.dissolve_texture.map(|t| t.id()));
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &dissolve_texture_binding)
+                        .as_ref(),
+                    *texture_unit as i32,
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(program, &format!("has_{}", dissolve_texture_binding))
+                        .as_ref(),
+                    self.dissolve_texture.is_some() as i32,
+                );
+            }
+
+            if let Some(illumination_model_binding) = &bindings.illumination_model {
+                gl.uniform_1_u32(
+                    gl.get_uniform_location(program, &illumination_model_binding)
+                        .as_ref(),
+                    self.illumination_model.unwrap_or_default() as u32,
+                );
+                gl.uniform_1_i32(
+                    gl.get_uniform_location(
+                        program,
+                        &format!("has_{}", illumination_model_binding),
+                    )
+                    .as_ref(),
+                    self.illumination_model.is_some() as i32,
+                );
             }
         }
     }
@@ -324,10 +557,13 @@ impl Model {
         vertex_binding: &str,
         normal_binding: Option<&str>,
         texture_binding: Option<&str>,
+        material_bindings: Option<&MaterialBindings>,
     ) {
         for mesh in &self.meshes {
             if let Some(material) = mesh.material {
-                self.material[material].bind(gl, program);
+                if let Some(m) = material_bindings {
+                    self.material[material].bind(gl, program, m);
+                }
             }
             mesh.draw(gl, program, vertex_binding, normal_binding, texture_binding);
         }
@@ -341,12 +577,15 @@ impl Model {
         vertex_binding: &str,
         normal_binding: Option<&str>,
         texture_binding: Option<&str>,
+        material_bindings: Option<&MaterialBindings>,
     ) {
         let Some(mesh) = self.meshes.get(mesh_idx) else {
             return;
         };
         if let Some(material) = mesh.material {
-            self.material[material].bind(gl, program);
+            if let Some(m) = material_bindings {
+                self.material[material].bind(gl, program, m);
+            }
         }
         mesh.draw(gl, program, vertex_binding, normal_binding, texture_binding);
     }
