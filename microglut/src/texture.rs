b88c00c12812ddebeeb6968defb33e4d5eab3ff8
KingTheFifth <johannes.kung@live.com>
2025-04-04 10:53:47 +0200

    Add support for loading textures with custom gl texture parameters

--- microglut/src/texture.rs
+++ microglut/src/texture.rs
@@ -58,6 +58,62 @@ impl Texture {
         }
     }
 
+    pub fn load_with_parameters(
+        gl: &Context,
+        data: &[u8],
+        int_params: &[(u32, i32)],
+        float_params: &[(u32, f32)],
+        gen_mipmap: bool,
+    ) -> Self {
+        use stb_image::image::{load_from_memory, LoadResult};
+        let image = match load_from_memory(data) {
+            LoadResult::Error(e) => panic!("{}", e),
+            LoadResult::ImageU8(image) => image,
+            LoadResult::ImageF32(_image) => todo!(),
+        };
+
+        unsafe {
+            let tex_id = gl.create_texture().unwrap();
+            gl.bind_texture(TEXTURE_2D, Some(tex_id));
+
+            for (parameter, value) in int_params {
+                gl.tex_parameter_i32(TEXTURE_2D, *parameter, *value);
+                print_error(gl, &format!("texture param {}", parameter)).unwrap();
+            }
+
+            for (parameter, value) in float_params {
+                gl.tex_parameter_f32(TEXTURE_2D, *parameter, *value);
+                print_error(gl, &format!("texture param {}", parameter)).unwrap();
+            }
+
+            let format = match image.depth {
+                1 => RED,
+                2 => RG,
+                3 => RGB,
+                4 => RGBA,
+                d => panic!("unsupported bit depth: {}", d),
+            };
+
+            gl.tex_image_2d(
+                TEXTURE_2D,
+                0,
+                RGBA as _,
+                image.width as _,
+                image.height as _,
+                0,
+                format as _,
+                UNSIGNED_BYTE,
+                Some(&image.data),
+            );
+            print_error(gl, "texture image_2d").unwrap();
+            if gen_mipmap {
+                gl.generate_mipmap(TEXTURE_2D);
+                print_error(gl, "texture gen mipmap").unwrap();
+            }
+            Texture { id: tex_id }
+        }
+    }
+
     pub fn id(&self) -> NativeTexture {
         self.id
     }
